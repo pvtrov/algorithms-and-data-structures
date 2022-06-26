d1727db17ad13e2502bc6a5e37c60794ff160701
mirabelka <aga.patro@gmail.com>
2022-06-26 23:16:08 +0200

    grredy bit 1.2

--- exercises from course/excercises from bit_/greedy/1_2_coin_change.py
+++ exercises from course/excercises from bit_/greedy/1_2_coin_change.py
@@ -4,3 +4,27 @@ Podaj algorytm, który obliczy, ile minimalnie monet trzeba użyć do wydania re
 Można założyć, że każdej monety mamy nieskończenie wiele sztuk.
 Czy algorytm zachłanny działa dla zestawu monet 1, 2, 7, 10? Jeśli tak, uzasadnij dlaczego. Jeśli nie, podaj kontrprzykład.
 """
+
+"""
+wybieramy co najwieksza monete i odejmujemy wartosc od X, i tak w kólko az X == 0
+"""
+
+
+def coin_change(X, coins):
+    coins.sort()
+    i = len(coins) - 1
+    coin = coins[i]
+    result = []
+
+    while X > 0:
+        while coin > X:
+            i -= 1
+            coin = coins[i]
+        X -= coin
+        result.append(coin)
+
+    return result
+
+
+coins = [7, 5, 3, 2, 1, 25, 4]
+print(coin_change(30, coins))
