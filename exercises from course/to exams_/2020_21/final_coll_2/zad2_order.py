a933dbb4eb07ad0f3822efaedc7515c3e8d44ca6
mirabelka <aga.patro@gmail.com>
2022-07-04 00:18:25 +0200

    kol zal 2 2020/21

--- exercises from course/to exams_/2020_21/final_coll_2/zad2_order.py
+++ exercises from course/to exams_/2020_21/final_coll_2/zad2_order.py
@@ -1,7 +1,7 @@
 from zad2testy import runtests
 """
-Na osi liczbowej znajduje się N punktów większych od M = 10 K . Z punktu A można przeskoczyć
-na punkt B wtedy i tylko wtedy gdy A%10 K == B//10 K . Proszę zaimplementować funkcję:
+Na osi liczbowej znajduje się N punktów większych od M = 10^K . Z punktu A można przeskoczyć
+na punkt B wtedy i tylko wtedy gdy A%10^K == B//10^K . Proszę zaimplementować funkcję:
 def order( L,K ):
 ...
 porządkującą punkty, tak aby możliwe było przejście od najwcześniejszego punktu w tym porządku,
@@ -17,12 +17,100 @@ L = [56,15,31,43,54,35,12,23], K = 1
 przykładowym, prawidłowym wynikiem jest lista:
 L = [12,23,31,15,54,43,35,56]
 """
+"""
+zauwazmy ze liczby to krawedzie, i pykamy cykl eulera, za chuj nie wiem czemu nie przechodzi dwoch ostatnich testow
+"""
 
-def order(L,K):
-    """tu prosze wpisac wlasna implementacje"""
-    return None
+
+def make_extra_edge(out_, in_):
+    extra, lack = None, None
+
+    for i in range(len(out_)):
+        diff = out_[i] - in_[i]
+        if diff == 0:
+            continue
+        elif diff == -1:
+            if extra is None:
+                extra = i
+        elif diff == 1:
+            if lack is None:
+                lack = i
+
+    if extra is None and lack is None:
+        return None
+    if extra is not None and lack is not None:
+        return (extra, lack)
+    return False
+
+
+def add_vertex(graph, x, y):
+    graph[x].append(y)
+
+
+def make_graph(numbers, K):
+    M = 10**K
+    edges = []
+    out_ = [0] * M
+    in_ = [0] * M
+    for i in range(len(numbers)):
+        x = numbers[i]%M
+        y = numbers[i]//M
+        edges.append((x, y))
+        out_[x] += 1
+        in_[y] += 1
+
+    extra_edge = make_extra_edge(out_, in_)
+
+    if extra_edge is not None:
+        edges.append(extra_edge)
+
+    graph = [[] for _ in range(M)]
+    for edge in edges:
+        add_vertex(graph, edge[0], edge[1])
+
+    return graph, edges, extra_edge
+
+
+def order(L, K):
+    graph, edges, extra_edge = make_graph(L, K)
+    res = []
+    s = [edges[0][0]]
+    while len(s) > 0:
+        u = s[-1]
+
+        if len(graph[u]) > 0:
+            # edge exists
+            w = graph[u][-1]
+            graph[u].pop()
+            s.append(w)
+        else:
+            # doesnt exist
+            s.pop()
+            res.append(u)
+
+    result = []
+    for i in range(len(res)-1):
+        one = str(res[i])
+        sec = str(res[i+1])
+        res_ = one + sec
+        res_ = int(res_)
+        result.append(res_)
+
+    added = extra_edge[1] * 10**K + extra_edge[0]
+    for i in range(len(result)):
+        if result[i] == added:
+            result = result[i+1:] + result[:i]
+            break
+
+    if len(result) == len(L):
+        return result
+    else:
+        return None
 
     
 runtests( order )
 
+# K = 1
+# L = [56, 15, 31, 43, 54, 35, 12, 23]
+# print(order(L, K))
 
--- exercises from course/to exams_/2020_21/final_coll_2/zad3_paths.py
+++ exercises from course/to exams_/2020_21/final_coll_2/zad3_paths.py
@@ -1,3 +1,6 @@
+from math import inf
+from queue import PriorityQueue
+
 from zad3testy import runtests
 """
 Dany jest nieskierowany graf G = (V, E) oraz dwa wierzchołki, s i t. Proszę zaimplementować
@@ -27,11 +30,66 @@ s = 0, t = 7
 funkcja powinna zwrócić wartość 7. Krawędzie 0-1, 1-4, 4-5, 5-7, 1-3, 3-6, 6-7.
 """
 
-def paths(G,s,t):
-    """tu prosze wpisac wlasna implementacje"""
-    return None
 
-    
-runtests( paths )
+def printpath(parent, i, t):
+    if parent[i] == -1:
+        t.append(i)
+        return t
+    t = printpath(parent, parent[i], t)
+    t.append(i)
+    return t
 
+
+def dijkstra_from_to(G, s, t):
+    n = len(G)
+    distance = [inf for _ in range(n)]
+    pre = [[] for _ in range(n)]
+    visited = [False for _ in range(n)]
+    visited[s] = True
+    pq = PriorityQueue()
+    distance[s] = 0
+
+    for i in range(n):
+        pq.put((distance[i], i))
+    while not pq.empty():
+        d, v = pq.get()
+        if v == t:
+            return pre, distance[t], distance
+
+        for u in G[v]:
+            if distance[u[0]] == d + u[1]:
+                pre[u[0]].append(v)
+            elif distance[u[0]] > d + u[1]:
+                distance[u[0]] = d + u[1]
+                pre[u[0]] = [v]
+                pq.put((distance[u[0]], u[0]))
+
+
+def make_paths(prevs, vertex, paths):
+    for neib in prevs[vertex]:
+        paths.add((vertex, neib))
+        make_paths(prevs, neib, paths)
+
+
+def paths(G, s, t):
+    parent, min_distance, distances = dijkstra_from_to(G, s, t)
+    path = set()
+    make_paths(parent, t, path)
+
+    return len(path)
+
+
+runtests(paths)
+# G =  [[(1,2),(2,4)],
+#       [(0,2),(3,11),(4,3)],
+#       [(0,4),(3,13)],
+#       [(1,11),(2,13),(5,17),(6,1)],
+#       [(1,3),(5,5)],
+#       [(3,17),(4,5),(7,7)],
+#       [(3,1),(7,3)],
+#       [(5,7),(6,3)] ]
+#
+# s = 0
+# t = 7
+# print(paths(G, s, t))
 
