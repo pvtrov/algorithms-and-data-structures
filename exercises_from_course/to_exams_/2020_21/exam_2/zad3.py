9b77dce226b70f3aa26261a0f258a00f72aa2e65
mirabelka <aga.patro@gmail.com>
2022-08-30 00:55:12 +0200

    lamps done

--- exercises_from_course/to_exams_/2020_21/exam_2/zad3.py
+++ exercises_from_course/to_exams_/2020_21/exam_2/zad3.py
@@ -1,11 +1,30 @@
 from zad3testy import runtests
 
 
-def lamps( n,T ):
-    """tu prosze wpisac wlasna implementacje"""
-    return -1
+def lamps(n, T):
+    max_ = 0
+    lamps_ = ["z"] * n
 
-    
-runtests( lamps )
+    sum_ = 0
+    for switch in T:
+        start = switch[0]
+        end = switch[1]
+        for lamp in range(start, end + 1):
+            if lamps_[lamp] == "z":
+                lamps_[lamp] = "c"
+            elif lamps_[lamp] == "c":
+                lamps_[lamp] = "n"
+                sum_ += 1
+            else:
+                lamps_[lamp] = "z"
+                sum_ -= 1
 
+        if max_ < sum_:
+            max_ = sum_
+
+    return max_
+
+
+runtests(lamps)
+# print(lamps(8, [(0, 4), (2, 6)]))
 
